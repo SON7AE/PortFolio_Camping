861158c53c470cf75b15a8d782f179a4a5abf867
SON7AE <bobsot0225@gmail.com>
2022-12-08 10:25:32 +0900

    캠핑 웹 만들기 - 4일차 - Detail Page 작업(오버레이 마무리)

--- src/components/atoms/detail/Map.vue
+++ src/components/atoms/detail/Map.vue
@@ -3,7 +3,44 @@
 </template>
 
 <script>
-export default {};
+import KakaoMapApiKey from '~/config/kakaoMapKey.js';
+import { onMounted } from '@vue/runtime-core';
+
+export default {
+    setup() {
+        onMounted(() => {
+            // 카카오 디벨로퍼 API KEY
+            const KAKAO_MAP_API_KEY = KakaoMapApiKey;
+
+            if (window.kakao && window.kakao.maps) {
+                initMap();
+            } else {
+                const script = document.createElement('script');
+                /* global kakao */
+                script.onload = () => kakao.maps.load(initMap);
+                script.src = `http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${KAKAO_MAP_API_KEY}`;
+                document.head.appendChild(script);
+            }
+        });
+        const initMap = () => {
+            const mapContainer = document.getElementById('map'); // 지도를 표시할 div
+            const mapOption = {
+                center: new kakao.maps.LatLng(33.451475, 126.570528), // 지도의 중심좌표
+                level: 3, // 지도의 확대 레벨
+            };
+            const map = new kakao.maps.Map(mapContainer, mapOption);
+            // 지도에 마커를 표시합니다.
+            const marker = new kakao.maps.Marker({
+                position: new kakao.maps.LatLng(33.451475, 126.570528), // 마커의 위치
+            });
+            // 마커가 지도 위에 표시되도록 설정합니다.
+            marker.setMap(map);
+            kakao.maps.event.addListener(marker, 'click', () => {});
+            // 아래 코드는 지도 위의 마커를 제거하는 코드입니다.
+            // marker.setMap(null);
+        };
+    },
+};
 </script>
 
 <style lang="scss" scoped>
---
+++ src/config/kakaoMapKey.js
@@ -0,0 +1,3 @@
+const KAKAO_MAP_API_KEY = '59b5e4ac3e73c5e16726784115fad033';
+
+export default KAKAO_MAP_API_KEY;
